71b3997b57d85a78d0a5923b19962c4a473ba01d
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-07-06 15:44:32 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Transactions from "./components/Transactions";
 function App() {
   return (
-    <div className="w-full bg-gray-800 h-screen">
+    <div className="w-full bg-gray-800 h-full">
       <div className="flex flex-wrap items-center justify-center min-h-[100vh] w-full">
-        <h1 className="text-white text-center w-full text-3xl ">
+        <h1 className="text-white text-center w-full text-3xl m-9">
           The list of Transactions of Customers
         </h1>
         <Transactions/>
--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -12,24 +12,27 @@ import { transactions } from "../constants/index"
 
 const Transactions = () => {
   return (
-    <Table className="bg-white ">
+    <div className="w-full flex justify-center items-center">
+    <div className="w-6/12 ">
+    <Table className="bg-white">
   <TableCaption>A list of transition of Customers.</TableCaption>
   <TableHeader>
     <TableRow>
       <TableHead className="w-[100px]">Customer</TableHead>
-      <TableHead className="text-right">Amount</TableHead>
+      <TableHead className="text-center">Amount</TableHead>
     </TableRow>
   </TableHeader>
   <TableBody>
     {transactions.map((item) => (
     <TableRow key={item.id}>
-      <TableCell className="font-medium">{item.customer}</TableCell>
-      <TableCell className="text-right">{item.amount}</TableCell>
+      <TableCell className="font-medium w-56">{item.customer}</TableCell>
+      <TableCell className="text-center">{item.amount}</TableCell>
     </TableRow>
     ))}
   </TableBody>
 </Table>
-
+</div>
+</div>
   )
 }
 
